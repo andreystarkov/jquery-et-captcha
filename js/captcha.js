106f96fb40f69335ac8110c728d73ca498cd5c09
Andrew <im@andreystarkov.ru>
2013-11-08 03:14:12 +0600

    stable beta

--- js/captcha.js
+++ js/captcha.js
@@ -3,8 +3,58 @@
          im@andreystarkov.ru | andreystarkov.ru
       */
 
-  (
-        function($){
+        // options
+
+        var colorYes = '#B6CD48';
+        var colorYesBorder = '#8b9947';
+
+        var colorNo = '#3498DB';
+        var colorNoBorder = '#2d678d';
+
+        function captchaSuccess(){
+
+            $('.robo-button').html("Так прекрасно, что ты не робот!");
+
+            $('.robo-button').animate({backgroundColor: colorYes, borderBottomColor: colorYesBorder, color: '#1F1613'}, function(){
+                $('.captcha-box').slideToggle();
+                $('.demo form').slideToggle();
+            });
+
+            $('.sortable').sortable('disable');
+        }
+
+        function captchaOnDrop(ui){
+             var i = ui.item.index()+1;
+                var n = ui.item.html();
+                var c = 0;
+                if( i == n ) {
+                    ui.item.animate({backgroundColor: colorYes, borderBottomColor: colorYesBorder}, function(){
+                       $('.sortable li').each( function(index){
+                        if( ($(this).index()+1) == $(this).html() ){
+
+                                $(this).animate({backgroundColor: colorYes, borderBottomColor: colorYesBorder}, function(){
+                                    c = c+1;
+                                    $('.good-count').html(c);
+                                    if(c == 6) captchaSuccess();
+                                });
+                            } else {
+                                $(this).animate({backgroundColor: colorNo, borderBottomColor: colorNoBorder});
+
+                            }
+                        });
+                    });
+                } else {
+                    i.item.animate({backgroundColor: colorNo, borderBottomColor: colorNoBorder});
+                }
+
+
+                if($('.good-count').html() == '6') {
+
+                }
+        }
+
+
+        (function($){
 
             $.fn.shuffle = function() {
                 return this.each(function(){
